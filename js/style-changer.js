379d38bb7e5f338947d39290866d0f2ae5d4c2d0
GOPAL KRUSHNA SAHOO <gopalkrushnas063@gmail.com>
2022-10-15 01:18:43 +0530

    Responsive with skin changer effect

---
+++ js/style-changer.js
@@ -0,0 +1,4 @@
+const styleChangerToggle = document.querySelector(".style-changer-toggle");
+styleChangerToggle.addEventListener("click",()=>{
+    document.querySelector(".styler-changer").classList.toggle("open");
+})
