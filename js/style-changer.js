0a105bfa10d39ad31131a9b105d110de0e430f25
GOPAL KRUSHNA SAHOO <103574856+gopalkrushnas063@users.noreply.github.com>
2022-11-25 19:05:02 +0530

    updated

--- js/style-changer.js
+++ js/style-changer.js
@@ -27,14 +27,14 @@ function setActiveStyle(color){
 
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click",()=>{
-    dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
     document.body.classList.toggle("dark");
 })
 window.addEventListener("load",()=>{
     if(document.body.classList.contains("dark")){
-        dayNight.querySelector("i").classList.add("fa-sun");
+        dayNight.querySelector("i").classList.add("fmoon");
     }else{
-        dayNight.querySelector("i").classList.add("fa-moon");
+        dayNight.querySelector("i").classList.add("fa-sun");
     }
 })
